b55c29fe9e7ccf68c970d6c1d0b297386c4c8fe5
Maitre Antho <178758626+MaitreAntho@users.noreply.github.com>
2025-03-09 18:44:14 +0100

    feat: Ajout des scripts serveur et des exports

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,9 +1,28 @@
-fx_version "adamant"
+fx_version "cerulean"
 lua54 "yes"
 game "gta5"
 author "MaitreAntho"
-description "Script qui permet d'afficher vos armes dans le dos, compatible qu'avec ox_inventory."
+description "WeaponsBack with Backpack System"
+version "1.0.0"
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'config.lua'
+}
 
 client_scripts {
     "Client/*.lua"
+}
+
+server_scripts {
+    "Server/*.lua"
+}
+
+dependencies {
+    'ox_inventory',
+    'ox_lib'
+}
+
+exports {
+    'backpack'
 }
